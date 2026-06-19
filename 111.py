d78c1ba3cf327c00019063feee0a947213746d6f
nikitanikonoff-ai <nikita.nikonoff@gmail.com>
2026-06-19 12:37:26 +0300

    blablabla

--- 111.py
+++ 111.py
@@ -1,8 +1,6 @@
 import datetime
 
-print("hello!")
-
-print("hello 111 and gjghjgh")
+print("hello! and hello 111 and gjghjgh")
 
 print("Our project: Git tutorial")
 print("Today: ", datetime.today().strftime("%d/%m/%Y"))
