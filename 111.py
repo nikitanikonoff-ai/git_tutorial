a31cbe21dd93e98bcc1519f58f202432b8780664
nikitanikonoff-ai <nikita.nikonoff@gmail.com>
2026-06-19 11:35:26 +0300

    add date

--- 111.py
+++ 111.py
@@ -1,6 +1,10 @@
+import datetime
+
 name = input("what's your name? ")
 print("hello " + name, "!")
 
 print("hello")
 print("111")
 print("gjghjgh")
+
+print("Today: ", datetime.today().strftime("%d/%m/%Y"))
