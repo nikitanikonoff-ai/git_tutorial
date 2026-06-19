c325c6e1a7d31e15ba302eeb2fd0067f403c8ee6
nikitanikonoff-ai <nikita.nikonoff@gmail.com>
2026-06-19 13:00:38 +0300

    deleted input

--- 111.py
+++ 111.py
@@ -2,6 +2,6 @@ import datetime
 
 print("github")
 print("tutorial")
-name = input("what's your name? ")
+
 print("Our project: Git tutorial")
 print("Today: ", datetime.today().strftime("%d/%m/%Y"))
