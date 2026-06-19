2e34d0a4b0c8184c7c9b04263d5a951770a98851
nikitanikonoff-ai <nikita.nikonoff@gmail.com>
2026-06-19 12:51:49 +0300

    added name input

--- 111.py
+++ 111.py
@@ -3,6 +3,6 @@ import datetime
 print("hello! and hello 111 and gjghjgh")
 print("github")
 print("tutorial")
-
+name = input("what's your name? ")
 print("Our project: Git tutorial")
 print("Today: ", datetime.today().strftime("%d/%m/%Y"))
